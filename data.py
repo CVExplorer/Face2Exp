af0b241e23a9bbc0b1b93707793ab402f3810191
CVExplorer <94514322+CVExplorer@users.noreply.github.com>
2022-03-18 21:19:55 +0800

    Update data.py

--- data.py
+++ data.py
@@ -10,30 +10,28 @@ from torchvision import transforms
 import torch.utils.data as data
 
 from augmentation import RandAugment
-from PIL import Image
 import random
 import copy
 
 logger = logging.getLogger(__name__)
 
+cifar10_mean = (0.4914, 0.4822, 0.4465)
+cifar10_std = (0.2471, 0.2435, 0.2616)
+cifar100_mean = (0.5071, 0.4867, 0.4408)
+cifar100_std = (0.2675, 0.2565, 0.2761)
 normal_mean = (0.5, 0.5, 0.5)
 normal_std = (0.5, 0.5, 0.5)
 aff_mean = (0.5863, 0.4595, 0.4030)
 aff_std = (0.2715, 0.2424, 0.2366)
 
-
-def get_10_balance(args):
+def get_data(args):
     transform_labeled = transforms.Compose([
         transforms.Resize(256),
         transforms.RandomCrop(args.resize),
         transforms.RandomHorizontalFlip(),
         transforms.RandomRotation(5),
-        #         transforms.RandomCrop(size=args.resize,
-        #                               padding=int(args.resize*0.125),
-        #                               padding_mode='reflect'),
-        #         transforms.CenterCrop(args.resize),
         transforms.ToTensor(),
-        # transforms.RandomErasing(p=1, scale=(0.02, 0.1), ratio=(0.3, 3.3), value=0, inplace=False),
+        transforms.RandomErasing(p=1, scale=(0.02, 0.1), ratio=(0.3, 3.3), value=0, inplace=False),
         transforms.Normalize(mean=aff_mean, std=aff_std)
     ])
     transform_val = transforms.Compose([
@@ -42,45 +40,7 @@ def get_10_balance(args):
         transforms.ToTensor(),
         transforms.Normalize(mean=aff_mean, std=aff_std)
     ])
-    path_meta_bal = '/home/cseadmin/MPL/labeled_10_balance.csv'
-    path_meta_train = '/home/data/lzy/AffectNet/Manually_Annotated_file_lists/training.csv'
-    path_meta_val = '/home/data/lzy/AffectNet/Manually_Annotated_file_lists/validation.csv'
-    orginal_dataset_dir = '/home/data/lzy/AffectNet/Manually_Annotated/Manually_Annotated_Images/'
-
-    DF_balance = pd.read_csv(path_meta_bal)
-
-    DF = pd.read_csv(path_meta_train)
-    DFselect = DF.loc[DF['expression'] < 8]
-
-    DF2 = pd.read_csv(path_meta_val)
-    DFselect2 = DF2.loc[DF2['expression'] < 8]
-
-    label_dataset = AfData(data=DF_balance, directory=orginal_dataset_dir, transform=transform_labeled)
-    unlabel_dataset = AfData(data=DFselect, directory=orginal_dataset_dir,
-                             transform=TransformMPL(args, mean=aff_mean, std=aff_std))
-    test_dataset = AfData(data=DFselect2, directory=orginal_dataset_dir, transform=transform_val)
-    return label_dataset, unlabel_dataset, test_dataset
-
-def get_10_balance_7(args):
-    transform_labeled = transforms.Compose([
-        transforms.Resize(256),
-        transforms.RandomCrop(args.resize),
-        transforms.RandomHorizontalFlip(),
-
-        #         transforms.RandomCrop(size=args.resize,
-        #                               padding=int(args.resize*0.125),
-        #                               padding_mode='reflect'),
-        #         transforms.CenterCrop(args.resize),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=aff_mean, std=aff_std)
-    ])
-    transform_val = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(args.resize),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=aff_mean, std=aff_std)
-    ])
-    path_meta_bal = '/home/cseadmin/MPL/labeled_10_balance.csv'
+    path_meta_bal = '../data/labeled_10_balance.csv'
     path_meta_train = '/home/data/lzy/AffectNet/Manually_Annotated_file_lists/training.csv'
     path_meta_val = '/home/data/lzy/AffectNet/Manually_Annotated_file_lists/validation.csv'
     orginal_dataset_dir = '/home/data/lzy/AffectNet/Manually_Annotated/Manually_Annotated_Images/'
@@ -101,6 +61,39 @@ def get_10_balance_7(args):
     return label_dataset, unlabel_dataset, test_dataset
 
 
+def get_raf_data(args):
+    transform_labeled = transforms.Compose([
+        transforms.Resize(256),
+        transforms.RandomCrop(args.resize),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(5),
+        transforms.ToTensor(),
+        transforms.RandomErasing(p=1, scale=(0.02, 0.1), ratio=(0.3, 3.3), value=0, inplace=False),
+        transforms.Normalize(mean=aff_mean, std=aff_std)
+    ])
+    transform_val = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(args.resize),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=aff_mean, std=aff_std)
+    ])
+
+    path_label='../data/raf_train.csv'
+    path_unlabel='../data/raf_aff_train.csv'
+    path_test='../data/raf_test.csv'
+    DF_label=pd.read_csv(path_label)
+    DF_unlabel=pd.read_csv(path_unlabel)
+    DF_test=pd.read_csv(path_test)
+    label_dataset_dir='/home/data/lzy/AffectNet/Manually_Annotated/Manually_Annotated_Images'
+    unlabel_dataset_dir='/home/data/lzy/AffectNet/Manually_Annotated/Manually_Annotated_Images'
+    test_dataset_dir='/home/data/lzy/AffectNet/Manually_Annotated/Manually_Annotated_Images'
+    label_dataset = RafData(data=DF_label, directory=label_dataset_dir, transform=transform_labeled)
+    unlabel_dataset = RafData(data=DF_unlabel, directory=unlabel_dataset_dir,
+                             transform=TransformMPL(args, mean=aff_mean, std=aff_std))
+    test_dataset = RafData(data=DF_test, directory=test_dataset_dir, transform=transform_val)
+    return label_dataset, unlabel_dataset, test_dataset
+
+
 class AfData(data.Dataset):
     def __init__(self, data, directory, transform):
         self.data = data
@@ -118,6 +111,19 @@ class AfData(data.Dataset):
         img = self.transform(cropped)
         return img, target
 
+class RafData(data.Dataset):
+    def __init__(self,data,directory,transform):
+        self.data = data
+        self.directory = directory
+        self.transform = transform
+    def __len__(self):
+        return len(self.data)
+    def __getitem__(self,idx):
+        path = os.path.join(self.directory, self.data.iloc[idx]['subDirectory_filePath'])   
+        target = self.data.iloc[idx]['expression']
+        image = Image.open(path).convert('RGB')
+        img = self.transform(image)
+        return img,target
 
 class TransformMPL(object):
     def __init__(self, args, mean, std):
@@ -159,7 +165,5 @@ class TransformMPL(object):
         return self.normalize(ori), self.normalize(aug)
 
 
-DATASET_GETTERS = {
-                   'bal': get_10_balance,
-                   'bal_7': get_10_balance_7,                   
-                   }
+DATASET_GETTERS = {'get_data': get_data,
+             'get_raf_data': get_raf_data}
